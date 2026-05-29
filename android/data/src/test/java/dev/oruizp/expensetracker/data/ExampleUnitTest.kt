8678ff4b511021628962864114109eec0af24e32
gatosempai <gatosempai@users.noreply.github.com>
2026-05-29 17:09:59 -0600

    Left Data modules files support added for Android App

--- android/data/src/test/java/dev/oruizp/expensetracker/data/ExampleUnitTest.kt
+++ android/data/src/test/java/dev/oruizp/expensetracker/data/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
-package dev.oruizp.android.domain
+package dev.oruizp.expensetracker.data
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
