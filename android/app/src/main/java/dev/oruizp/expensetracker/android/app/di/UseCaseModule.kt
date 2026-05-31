54424b2d08ffafe7b48aeff3bc1410a23b24e496
gatosempai <gatosempai@users.noreply.github.com>
2026-05-30 18:48:11 -0600

    Koin adjustments added for Android App

--- android/app/src/main/java/dev/oruizp/expensetracker/android/app/di/UseCaseModule.kt
+++ android/app/src/main/java/dev/oruizp/expensetracker/android/app/di/UseCaseModule.kt
@@ -1,10 +1,12 @@
 package dev.oruizp.expensetracker.android.app.di
 
 import dev.oruizp.expensetracker.android.domain.usecases.expense.AddExpenseUseCase
+import dev.oruizp.expensetracker.android.domain.usecases.expense.GetExpensesUseCase
 import dev.oruizp.expensetracker.android.domain.usecases.expense.GetTotalExpenseUseCase
 import org.koin.dsl.module
 
 val useCaseModule = module {
     factory { GetTotalExpenseUseCase(get()) }
     factory { AddExpenseUseCase(get()) }
+    factory { GetExpensesUseCase(get()) }
 }
--- android/app/src/main/java/dev/oruizp/expensetracker/android/app/di/ViewModelModule.kt
+++ android/app/src/main/java/dev/oruizp/expensetracker/android/app/di/ViewModelModule.kt
@@ -5,5 +5,11 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    viewModel { ExpenseViewModel(get(), get(), get()) }
+    viewModel {
+        ExpenseViewModel(
+            get(),
+            get(),
+            get()
+        )
+    }
 }
